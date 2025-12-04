302bab039dbe859952f0a0d82af2ac8be9b237dd
Kian <122436242+kian-al@users.noreply.github.com>
2025-12-04 18:27:02 +0330

    refactor: update backend structure

---
+++ back-end/src/test/java/ir/library/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.library.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
